e9cc48b48a585e11f8fb3652b4e59ecc8e5a7c64
Luis <luismartinezmilla@gmail.com>
2022-02-10 14:29:48 +0100

    gh

--- gestion-empleados-ios/LoginViewController.swift
+++ gestion-empleados-ios/LoginViewController.swift
@@ -13,12 +13,13 @@ import Alamofire
 
 class LoginViewController: ViewController {
     
-    struct Data: Codable {
+    struct Data: Decodable { //esto se tiene que llamar como en el response (las variables)
         let api_token: String
-        let rol: String
+        let msg: String
     }
-    var api_token:String = ""
-    var rol:String?
+    var api_token:String?
+    //var rol:String?
+    var msg:String?
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     
@@ -31,45 +32,41 @@ class LoginViewController: ViewController {
     
     @IBAction func loginBotonPulsado(_ sender: Any) {
         //email.text
-        let url = "http://localhost/proyectos/gestion-empleados/public/api/users/login"
+        let url = "http://192.168.64.3/proyectos/gestion-empleados/public/api/users/login"
         let body = ["email": email.text, "password": password.text]
         
+        //AF.req encoding
         AF.request(url, method: .put, parameters: body, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: Data.self){response in
-            self.api_token = response.value!.api_token
-            self.rol = response.value!.rol
+            print(response)
+            self.api_token = response.value?.api_token
+            self.msg = response.value?.msg
+            //self.rol = response.value?.rol
         }
-        print(api_token)
-        if rol == "empleado"{
-            performSegue(withIdentifier: "logueado_perfil", sender: nil)
-        }else{
-            performSegue(withIdentifier: "logueado_lista", sender: nil)
-        }
-        print(api_token)
+        print("Tu api token es ",api_token)
+        print(msg)
+//        if rol == "empleado"{
+//            performSegue(withIdentifier: "logueado_perfil", sender: nil)
+//        }else{
+//            performSegue(withIdentifier: "logueado_lista", sender: nil)
+//        }
         
-        //performSegue(withIdentifier: "logueado", sender: nil)
+        
+       // performSegue(withIdentifier: "logueado", sender: nil)
     }
     
     //func prepare for segue
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "logueado_lista"{
-            let listaVista = segue.destination as! ListaViewController
-            listaVista.api_token = api_token
-        }
-        if segue.identifier == "logueado_perfil"{
-            let listaPerfil = segue.destination as! PerfilViewController
-            listaPerfil.api_token = api_token
-        }
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        if segue.identifier == "logueado_lista"{
+//            let listaVista = segue.destination as! ListaViewController
+//            listaVista.api_token = api_token!
+//        }
+//        if segue.identifier == "logueado_perfil"{
+//            let listaPerfil = segue.destination as! PerfilViewController
+//            listaPerfil.api_token = api_token!
+//        }
+//    }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
