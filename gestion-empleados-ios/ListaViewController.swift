af42109c2e06332510420d992f9048ad74edb863
Luis <luismartinezmilla@gmail.com>
2022-02-15 13:28:30 +0100

    bhjvhjhj

--- gestion-empleados-ios/ListaViewController.swift
+++ gestion-empleados-ios/ListaViewController.swift
@@ -8,17 +8,19 @@
 import UIKit
 import Alamofire
 
-class ListaViewController: ViewController {
+class ListaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var api_token:String? = ""
     var rol:String? = ""
     var id:Int? = nil
     
+    @IBOutlet weak var tablaEmpleados: UITableView!
+    
     struct Usuario: Encodable, Decodable {
-        let id: Int?
-        let nombre: String?
-        let puesto: String?
-        let salario: Int?
+        let id: Int
+        let nombre: String
+        let puesto: String
+        let salario: Int
     }
 
     struct UsuarioR: Encodable, Decodable{
@@ -29,32 +31,43 @@ class ListaViewController: ViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tablaEmpleados.dataSource = self
+        tablaEmpleados.delegate = self
         print("entra a la lista")
         print(rol)
         
         let url = "http://192.168.64.3/proyectos/gestion-empleados/public/api/list"
         let body = ["api_token": api_token]
-        
+        print("apitoken", api_token)
         //AF.req encoding
         AF.request(url, method: .put, parameters: body, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: UsuarioR.self){response in
-            print(response)
+            print("response de lista",response)
             self.usuarios = response.value?.usuarios
-            print(self.usuarios)
+            print("lista de usuarios:",self.usuarios)
+            
+            self.tablaEmpleados.reloadData()
         }
+        
+        
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usuarios?.count ?? 0
+//        return 10
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath) as! EmpleadoTableViewCell
-        celda.NombreTXT.text = self.usuarios![indexPath.row].nombre!
-        celda.PuestoTXT.text = self.usuarios![indexPath.row].puesto!
-        celda.SalarioTXT.text = String(self.usuarios![indexPath.row].salario!)
+        var celda = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath) as! EmpleadoTableViewCell
+        celda.NombreTXT.text = self.usuarios![indexPath.row].nombre
+        celda.PuestoTXT.text = self.usuarios![indexPath.row].puesto
+        celda.SalarioTXT.text = String(self.usuarios![indexPath.row].salario)
+        
+//        celda.NombreTXT.text = "paco"
+//        celda.PuestoTXT.text = "albañil"
+//        celda.SalarioTXT.text = "9999"
 
         return celda
-        }
+    }
         
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //segue a perfil
